9f4d00b74d7fe29e6fe380868e5c8a68d4a7ce66
Erik Laucks <phisigmapi314@gmail.com>
2019-02-12 13:51:02 -0500

    Added readline for inline editing of commands

--- src/main.c
+++ src/main.c
@@ -4,8 +4,10 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-void execCommand(char **tokens, int numTokens);
+void execCommand(char **tokens, int numTokens, int *fd, int pipedInput);
 void handleCommand(char *input);
 void handleEnv(char *inupt, int index);
 int countTokens(char *input);
@@ -16,38 +18,27 @@ extern char **environ;
 int main(int argc, char *argv[]) {
   char *title = getenv("lshprompt") ? getenv("lshprompt") : "lsh>";
   size_t size = 1;
-  char *input = (char *) malloc(size + 1);
+  char *input;
 
+  input = readline(title);
   while(strcmp(input, "exit") != 0) {
-    printf("%s", title);
-    input = (char *) realloc(input, size + 1);
-    int length = getline(&input, &size, stdin);
-    input[length - 1] = '\0';
-
     int index = strcspn(input, "=");
     if (index < strlen(input)) {
       handleEnv(input, index);
     } else {
       handleCommand(input);
     }
+
+    input = readline(title);
   }
 
   return 0;
 }
 
-void execCommand (char **tokens, int numTokens) {
+void execCommand (char **tokens, int numTokens, int *fd, int pipedInput) {
   // printf("Num tokens: %d\n", numTokens);
   // for (int i = 0; i < numTokens; i++) {
   //   printf("Token:%s\n", tokens[i]);
-  // }
-
-  // printf("%d %d %d\n", pipedInput, fd[0], fd[1]);
-  // if (pipedInput >= 0) {
-  //   if (pipedInput == 0) dup2(fd[1], 1);
-  //   if (pipedInput == 1) dup2(fd[0], 0);
-  //
-  //   close(fd[0]);
-  //   close(fd[1]);
   // }
 
   for (int i = 0; i < numTokens; i++) {
@@ -63,12 +54,19 @@ void execCommand (char **tokens, int numTokens) {
     exit(0);
   }
   if (pid == 0) {
-    // dup2(1, 0);
+    // printf("%d %d %d\n", pipedInput, fd[0], fd[1]);
+    if (pipedInput >= 0) {
+      if (pipedInput == 0) dup2(fd[1], 1);
+      if (pipedInput == 1) dup2(fd[0], 0);
+
+      close(fd[0]);
+      close(fd[1]);
+    }
+
     int ret = execvpe(tokens[0], tokens, environ);
     if (ret < 0) printf("lsh: %s: command not found\n", tokens[0]);
     exit(0);
   }
-  waitpid(pid, 0, 0);
 }
 
 void handleCommand (char *input) {
@@ -81,22 +79,26 @@ void handleCommand (char *input) {
   char *tokens[numTokens + 1];
   tokenize(input_copy, numTokens, tokens);
 
-  // int fd[2];
+  int fd[2];
   int index = 0;
   while (index < numTokens) {
     if (strcmp(tokens[index], "|") == 0) {
       tokens[index] = NULL;
 
-      // pipe(fd);
-      execCommand(tokens, index);
-      execCommand(&tokens[index + 1], numTokens - index - 1);
+      pipe(fd);
+      execCommand(&tokens[index + 1], numTokens - index - 1, fd, 1);
+      execCommand(tokens, index, fd, 0);
+      close(fd[0]);
+      close(fd[1]);
+
+      while (wait(NULL) > 0);
       return;
     }
     index++;
   }
 
-  execCommand(tokens, numTokens);
-
+  execCommand(tokens, numTokens, fd, -1);
+  wait(NULL);
   return;
 }
 
